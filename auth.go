87fcd3763140ab5f084c8183b25594a0c3b8f7cf
Mac <mohammed.ali.chherawalla@gmail.com>
2020-04-10 12:55:58 +0530

    upadting auth.go

--- auth.go
+++ auth.go
@@ -1,10 +1,5 @@
 package goboiler
 
-import (
-	"github.com/labstack/echo"
-	"github.com/wednesday-solution/go-boiler/models"
-)
-
 // AuthToken holds authentication token details with refresh token
 type AuthToken struct {
 	Token        string `json:"token"`
@@ -14,15 +9,4 @@ type AuthToken struct {
 // RefreshToken holds authentication token details
 type RefreshToken struct {
 	Token string `json:"token"`
-}
-
-// RBACService represents role-based access control service interface
-type RBACService interface {
-	User(echo.Context) models.User
-	EnforceRole(echo.Context, int) error
-	EnforceUser(echo.Context, int) error
-	EnforceCompany(echo.Context, int) error
-	EnforceLocation(echo.Context, int) error
-	AccountCreate(echo.Context, int, int, int) error
-	IsLowerRole(echo.Context, int) error
 }
