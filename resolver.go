191fb0a09606f04bd6551c50b8f8e15135555b94
praveenchand-wed <praveen@wednesday.is>
2020-09-25 23:54:27 +0530

    fixing resolver

--- resolver.go
+++ resolver.go
@@ -2,16 +2,6 @@
 // THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
 package goboiler
 
-import (
-	"database/sql"
-)
-
 // Resolver ...
 type Resolver struct {
-	db *sql.DB
 }
-
-const inputKey = "input"
-
-type mutationResolver struct{ *Resolver }
-type queryResolver struct{ *Resolver }
