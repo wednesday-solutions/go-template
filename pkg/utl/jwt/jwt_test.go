3def7c8d1a4e2f2ec27ea32c45bee9abc7667cfa
praveenchand-wed <praveen@wednesday.is>
2020-09-29 20:06:00 +0530

    add test for utl

--- pkg/utl/jwt/jwt_test.go
+++ pkg/utl/jwt/jwt_test.go
@@ -11,6 +11,52 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestNew(t *testing.T) {
+	cases := map[string]struct {
+		algo         string
+		secret       string
+		minSecretLen int
+		req          models.User
+		wantErr      bool
+		want         jwt.Service
+	}{
+		"invalid algo": {
+			algo:    "invalid",
+			wantErr: true,
+		},
+		"invalid secret length": {
+			algo:    "HS256",
+			secret:  "123",
+			wantErr: true,
+		},
+		"invalid secret length with min defined": {
+			algo:         "HS256",
+			minSecretLen: 4,
+			secret:       "123",
+			wantErr:      true,
+		},
+		"success": {
+			algo:         "HS256",
+			secret:       "g0r$kt3$t1ng",
+			minSecretLen: 1,
+			req: models.User{
+				Username:   null.StringFrom("johndoe"),
+				Email:      null.StringFrom("johndoe@mail.com"),
+				CompanyID:  null.IntFrom(1),
+				LocationID: null.IntFrom(1),
+			},
+			want: jwt.Service{},
+		},
+	}
+
+	for name, tt := range cases {
+		t.Run(name, func(t *testing.T) {
+			_, err := jwt.New(tt.algo, tt.secret, 60, tt.minSecretLen)
+			assert.Equal(t, tt.wantErr, err != nil)
+		})
+	}
+}
+
 func TestGenerateToken(t *testing.T) {
 	cases := map[string]struct {
 		algo         string
