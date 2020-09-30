33003912042a8462639c4841a5d5c6154077bff3
praveenchand-wed <praveen@wednesday.is>
2020-09-30 12:31:45 +0530

    update readme

---
+++ pkg/utl/result_wrapper/error_test.go
@@ -0,0 +1,68 @@
+package resultwrapper_test
+
+import (
+	"github.com/stretchr/testify/assert"
+	resultwrapper "github.com/wednesday-solutions/go-boiler/pkg/utl/result_wrapper"
+	"testing"
+)
+
+func TestSplitByLabel(t *testing.T) {
+
+	cases := []struct {
+		name     string
+		req      string
+		wantResp string
+	}{
+		{
+			name:     "Error string",
+			req:      "no rows in sql",
+			wantResp: "no rows in sql",
+		},
+		{
+			name:     "Having `Error` in string",
+			req:      `"Error":{"msg"}`,
+			wantResp: "\":{\"msg\"}",
+		},
+		{
+			name:     "Having `message` in string",
+			req:      `"message":{"msg"}`,
+			wantResp: "\":{\"msg\"}",
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			resp := resultwrapper.SplitByLabel(tt.req)
+			if len(tt.wantResp) != 0 {
+				assert.Equal(t, tt.wantResp, resp)
+			}
+		})
+	}
+}
+
+func TestErrorFormatter(t *testing.T) {
+
+	cases := []struct {
+		name     string
+		req      string
+		wantResp resultwrapper.ErrorMsg
+	}{
+		{
+			name:     "No string",
+			req:      "",
+			wantResp: resultwrapper.ErrorMsg{Errors: []string{""}},
+		},
+		{
+			name:     "Having Error string",
+			req:      `error message`,
+			wantResp: resultwrapper.ErrorMsg{Errors: []string{"error message"}},
+		},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			resp := resultwrapper.ErrorFormatter(tt.req)
+			assert.Equal(t, tt.wantResp, resp)
+		})
+	}
+}
