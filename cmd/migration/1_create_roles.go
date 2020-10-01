e8e948fe2e7edd709057912a5f6b9af512792570
praveenchand-wed <praveen@wednesday.is>
2020-10-01 15:17:43 +0530

    all migrations in loop

--- cmd/migration/1_create_roles.go
+++ cmd/migration/1_create_roles.go
@@ -7,41 +7,27 @@ import (
 )
 
 func init() {
-	var tableName = "roles"
-	var createTableQuery = fmt.Sprintf(`CREATE TABLE public.%s (
+
+	tableNames := []string{"roles", "companies", "locations", "users", "posts", "comments", "followers"}
+
+	var createTableQueries = []string{
+		fmt.Sprintf(`CREATE TABLE public.%s (
 			id SERIAL UNIQUE PRIMARY KEY,
 			access_level int NOT NULL,
 			name text  NOT NULL,
 			created_at TIMESTAMP WITH TIME ZONE,
 			updated_at TIMESTAMP WITH TIME ZONE,
 			deleted_at TIMESTAMP WITH TIME ZONE
-		);`, tableName)
-	migrations.MustRegister(func(db migrations.DB) error {
-		err := CreateTriggerForUpdatedAt(db)
-		if err != nil {
-			return err
-		}
-		err = createTable(db, createTableQuery, tableName)
-		if err != nil {
-			return err
-		}
-
-		tableName = "companies"
-		createTableQuery = fmt.Sprintf(`CREATE TABLE public.%s (
+		);`, tableNames[0]),
+		fmt.Sprintf(`CREATE TABLE public.%s (
 			id SERIAL UNIQUE PRIMARY KEY,
 			created_at TIMESTAMP WITH TIME ZONE,
 			updated_at TIMESTAMP WITH TIME ZONE,
 			deleted_at TIMESTAMP WITH TIME ZONE,
 			name text,
 			active boolean
-		);`, tableName)
-		err = createTable(db, createTableQuery, tableName)
-		if err != nil {
-			return err
-		}
-
-		tableName = "locations"
-		createTableQuery = fmt.Sprintf(`CREATE TABLE public.%s (
+		);`, tableNames[1]),
+		fmt.Sprintf(`CREATE TABLE public.%s (
 			id SERIAL UNIQUE PRIMARY KEY,
 			created_at TIMESTAMP WITH TIME ZONE,
 			updated_at TIMESTAMP WITH TIME ZONE,
@@ -50,14 +36,8 @@ func init() {
 			active BOOLEAN,
 			address TEXT,
 			company_id SERIAL REFERENCES companies(id)
-		);`, tableName)
-		err = createTable(db, createTableQuery, tableName)
-		if err != nil {
-			return err
-		}
-
-		tableName = "users"
-		createTableQuery = fmt.Sprintf(`CREATE TABLE public.%s (
+		);`, tableNames[2]),
+		fmt.Sprintf(`CREATE TABLE public.%s (
 				id SERIAL UNIQUE PRIMARY KEY,
 				created_at TIMESTAMP WITH TIME ZONE,
 				updated_at TIMESTAMP WITH TIME ZONE,
@@ -77,14 +57,8 @@ func init() {
 				role_id int REFERENCES roles(id),
 				company_id int REFERENCES companies(id),
 				location_id int REFERENCES locations(id)
-			);`, tableName)
-		err = createTable(db, createTableQuery, tableName)
-		if err != nil {
-			return err
-		}
-
-		tableName = "posts"
-		createTableQuery = fmt.Sprintf(`CREATE TABLE IF NOT EXISTS public.%s (
+			);`, tableNames[3]),
+		fmt.Sprintf(`CREATE TABLE IF NOT EXISTS public.%s (
 			id SERIAL PRIMARY KEY,
 			user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 			title VARCHAR(200) NOT NULL,
@@ -92,14 +66,8 @@ func init() {
 			created_at TIMESTAMP WITH TIME ZONE,
 			updated_at TIMESTAMP WITH TIME ZONE,
 			deleted_at TIMESTAMP WITH TIME ZONE
-			);`, tableName)
-		err = createTable(db, createTableQuery, tableName)
-		if err != nil {
-			return err
-		}
-
-		tableName = "comments"
-		createTableQuery = fmt.Sprintf(`CREATE TABLE IF NOT EXISTS public.%s (	
+			);`, tableNames[4]),
+		fmt.Sprintf(`CREATE TABLE IF NOT EXISTS public.%s (	
 			id SERIAL PRIMARY KEY,	
 			user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,	
 			post_id INTEGER NOT NULL REFERENCES posts(id) ON DELETE CASCADE,	
@@ -108,27 +76,29 @@ func init() {
 			created_at TIMESTAMP WITH TIME ZONE,	
 			updated_at TIMESTAMP WITH TIME ZONE,	
 			deleted_at TIMESTAMP WITH TIME ZONE	
-			);`, tableName)
-		err = createTable(db, createTableQuery, tableName)
-		if err != nil {
-			return err
-		}
-
-		tableName = "followers"
-		createTableQuery = fmt.Sprintf(`CREATE TABLE IF NOT EXISTS public.%s (
+			);`, tableNames[5]),
+		fmt.Sprintf(`CREATE TABLE IF NOT EXISTS public.%s (
 			id SERIAL NOT NULL PRIMARY KEY,
 			follower_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 			followee_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 			created_at TIMESTAMP WITH TIME ZONE,
 			updated_at TIMESTAMP WITH TIME ZONE,
 			deleted_at TIMESTAMP WITH TIME ZONE
-			);`, tableName)
+			);`, tableNames[6]),
+	}
 
-		err = createTable(db, createTableQuery, tableName)
-		return err
-	}, func(db migrations.DB) error {
-		return DropTable(db, tableName)
-	})
+	for i := 0; i < len(tableNames); i++ {
+		migrations.MustRegister(func(db migrations.DB) error {
+			err := CreateTriggerForUpdatedAt(db)
+			if err != nil {
+				return err
+			}
+			err = createTable(db, createTableQueries[i], tableNames[i])
+			return err
+		}, func(db migrations.DB) error {
+			return DropTable(db, tableNames[i])
+		})
+	}
 }
 
 func createTable(db migrations.DB, createTableQuery string, tableName string) error {
