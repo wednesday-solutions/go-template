eb96a0629c08599886ae69bb7c527d3cac0068cc
praveenchand-wed <praveen@wednesday.is>
2020-10-01 15:49:20 +0530

    add struct for table name and query

--- cmd/migration/1_create_roles.go
+++ cmd/migration/1_create_roles.go
@@ -6,28 +6,37 @@ import (
 	"github.com/go-pg/migrations/v7"
 )
 
+type tableQuery struct {
+	Name  string
+	Query string
+}
+
 func init() {
 
-	tableNames := []string{"roles", "companies", "locations", "users", "posts", "comments", "followers"}
-
-	var createTableQueries = []string{
-		fmt.Sprintf(`CREATE TABLE public.%s (
+	tableQueries := []tableQuery{
+		{
+			Name: "roles",
+			Query: fmt.Sprintf(`CREATE TABLE public.%s (
 			id SERIAL UNIQUE PRIMARY KEY,
 			access_level int NOT NULL,
 			name text  NOT NULL,
 			created_at TIMESTAMP WITH TIME ZONE,
 			updated_at TIMESTAMP WITH TIME ZONE,
 			deleted_at TIMESTAMP WITH TIME ZONE
-		);`, tableNames[0]),
-		fmt.Sprintf(`CREATE TABLE public.%s (
+		);`, "roles"),
+		}, {
+			Name: "companies",
+			Query: fmt.Sprintf(`CREATE TABLE public.%s (
 			id SERIAL UNIQUE PRIMARY KEY,
 			created_at TIMESTAMP WITH TIME ZONE,
 			updated_at TIMESTAMP WITH TIME ZONE,
 			deleted_at TIMESTAMP WITH TIME ZONE,
 			name text,
 			active boolean
-		);`, tableNames[1]),
-		fmt.Sprintf(`CREATE TABLE public.%s (
+		);`, "companies"),
+		}, {
+			Name: "locations",
+			Query: fmt.Sprintf(`CREATE TABLE public.%s (
 			id SERIAL UNIQUE PRIMARY KEY,
 			created_at TIMESTAMP WITH TIME ZONE,
 			updated_at TIMESTAMP WITH TIME ZONE,
@@ -36,8 +45,10 @@ func init() {
 			active BOOLEAN,
 			address TEXT,
 			company_id SERIAL REFERENCES companies(id)
-		);`, tableNames[2]),
-		fmt.Sprintf(`CREATE TABLE public.%s (
+		);`, "locations"),
+		}, {
+			Name: "users",
+			Query: fmt.Sprintf(`CREATE TABLE public.%s (
 				id SERIAL UNIQUE PRIMARY KEY,
 				created_at TIMESTAMP WITH TIME ZONE,
 				updated_at TIMESTAMP WITH TIME ZONE,
@@ -57,8 +68,10 @@ func init() {
 				role_id int REFERENCES roles(id),
 				company_id int REFERENCES companies(id),
 				location_id int REFERENCES locations(id)
-			);`, tableNames[3]),
-		fmt.Sprintf(`CREATE TABLE IF NOT EXISTS public.%s (
+			);`, "users"),
+		}, {
+			Name: "posts",
+			Query: fmt.Sprintf(`CREATE TABLE IF NOT EXISTS public.%s (
 			id SERIAL PRIMARY KEY,
 			user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 			title VARCHAR(200) NOT NULL,
@@ -66,8 +79,10 @@ func init() {
 			created_at TIMESTAMP WITH TIME ZONE,
 			updated_at TIMESTAMP WITH TIME ZONE,
 			deleted_at TIMESTAMP WITH TIME ZONE
-			);`, tableNames[4]),
-		fmt.Sprintf(`CREATE TABLE IF NOT EXISTS public.%s (	
+			);`, "posts"),
+		}, {
+			Name: "comments",
+			Query: fmt.Sprintf(`CREATE TABLE IF NOT EXISTS public.%s (	
 			id SERIAL PRIMARY KEY,	
 			user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,	
 			post_id INTEGER NOT NULL REFERENCES posts(id) ON DELETE CASCADE,	
@@ -76,32 +91,35 @@ func init() {
 			created_at TIMESTAMP WITH TIME ZONE,	
 			updated_at TIMESTAMP WITH TIME ZONE,	
 			deleted_at TIMESTAMP WITH TIME ZONE	
-			);`, tableNames[5]),
-		fmt.Sprintf(`CREATE TABLE IF NOT EXISTS public.%s (
+			);`, "comments"),
+		}, {
+			Name: "followers",
+			Query: fmt.Sprintf(`CREATE TABLE IF NOT EXISTS public.%s (
 			id SERIAL NOT NULL PRIMARY KEY,
 			follower_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 			followee_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 			created_at TIMESTAMP WITH TIME ZONE,
 			updated_at TIMESTAMP WITH TIME ZONE,
 			deleted_at TIMESTAMP WITH TIME ZONE
-			);`, tableNames[6]),
+			);`, "followers"),
+		},
 	}
 
 	migrations.MustRegister(func(db migrations.DB) error {
-		for i := 0; i < len(tableNames); i++ {
+		for i := 0; i < len(tableQueries); i++ {
 			err := CreateTriggerForUpdatedAt(db)
 			if err != nil {
 				return err
 			}
-			err = createTable(db, createTableQueries[i], tableNames[i])
+			err = createTable(db, tableQueries[i].Query, tableQueries[i].Name)
 			if err != nil {
 				return err
 			}
 		}
 		return nil
 	}, func(db migrations.DB) error {
-		for i := len(tableNames) - 1; i >= 0; i-- {
-			err := DropTable(db, tableNames[i])
+		for i := len(tableQueries) - 1; i >= 0; i-- {
+			err := DropTable(db, tableQueries[i].Name)
 			if err != nil {
 				return err
 			}
