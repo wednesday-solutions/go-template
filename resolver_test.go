bcc1f2087fd1317b43becf16a7f7b9df8c39150f
praveenchand-wed <praveen@wednesday.is>
2020-10-13 20:27:32 +0530

    remove table location, company

--- resolver_test.go
+++ resolver_test.go
@@ -26,6 +26,66 @@ var (
 	userKey key = "user"
 )
 
+func TestCreateRole(t *testing.T) {
+	cases := []struct {
+		name     string
+		req      fm.RoleCreateInput
+		wantResp *fm.RolePayload
+		wantErr  bool
+	}{
+		{
+			name:    "Fail on Create role",
+			req:     fm.RoleCreateInput{},
+			wantErr: true,
+		},
+		{
+			name:     "Success",
+			req:      fm.RoleCreateInput{Name: "Role", AccessLevel: 100},
+			wantResp: &fm.RolePayload{Role: &fm.Role{Name: "Role", AccessLevel: 100}},
+			wantErr:  false,
+		},
+	}
+
+	resolver := gotemplate.Resolver{}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			err := godotenv.Load(fmt.Sprintf(".env.%s", os.Getenv("ENVIRONMENT_NAME")))
+			if err != nil {
+				fmt.Print("Error loading .env file")
+			}
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			oldDB := boil.GetDB()
+			defer func() {
+				db.Close()
+				boil.SetDB(oldDB)
+			}()
+			boil.SetDB(db)
+
+			if tt.name == "Fail on Create role" {
+				// insert new user
+				mock.ExpectQuery(regexp.QuoteMeta("INSERT INTO \"roles\" (\"access_level\",\"name\",\"created_at\",\"updated_at\",\"deleted_at\") VALUES ($1,$2,$3,$4,$5)")).
+					WithArgs().
+					WillReturnError(fmt.Errorf(""))
+			}
+			// insert new user
+			rows := sqlmock.NewRows([]string{"id"}).AddRow(1)
+			mock.ExpectQuery(regexp.QuoteMeta("INSERT INTO \"roles\" (\"access_level\",\"name\",\"created_at\",\"updated_at\",\"deleted_at\") VALUES ($1,$2,$3,$4,$5)")).
+				WithArgs().
+				WillReturnRows(rows)
+
+			c := context.Background()
+			response, err := resolver.Mutation().CreateRole(c, tt.req)
+			if tt.wantResp != nil {
+				assert.Equal(t, tt.wantResp, response)
+			}
+			assert.Equal(t, tt.wantErr, err != nil)
+		})
+	}
+}
+
 func TestLogin(t *testing.T) {
 	type args struct {
 		UserName string
