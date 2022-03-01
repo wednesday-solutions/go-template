060f029316d27d2019024d5f02caa39740803f55
Mac <mohammed.ali.chherawalla@gmail.com>
2022-03-01 23:46:23 +0530

    fix auth middleware bug

--- internal/middleware/auth/auth.go
+++ internal/middleware/auth/auth.go
@@ -115,7 +115,7 @@ func GraphQLMiddleware(
 
 			isSuperAdmin := false
 
-			if claims["role"].(string) == "ADMIN" {
+			if claims["role"].(string) == "SUPER_ADMIN" {
 				isSuperAdmin = true
 			}
 			if !isSuperAdmin {
--- internal/middleware/auth/auth_test.go
+++ internal/middleware/auth/auth_test.go
@@ -57,7 +57,7 @@ func TestGraphQLMiddleware(t *testing.T) {
 			wantStatus:       http.StatusOK,
 			err:              "",
 			tokenParser: func(token string) (*jwt.Token, error) {
-				return testutls.MockJwt("ADMIN"), nil
+				return testutls.MockJwt("SUPER_ADMIN"), nil
 			},
 			operationHandler: func(ctx context.Context) graphql2.ResponseHandler {
 				user := ctx.Value(auth.UserCtxKey).(*models.User)
@@ -154,7 +154,7 @@ func TestGraphQLMiddleware(t *testing.T) {
 			wantStatus:       http.StatusOK,
 			err:              "No user found for this email address",
 			tokenParser: func(token string) (*jwt.Token, error) {
-				return testutls.MockJwt("ADMIN"), nil
+				return testutls.MockJwt("SUPER_ADMIN"), nil
 			},
 			operationHandler: func(ctx context.Context) graphql2.ResponseHandler {
 				return nil
