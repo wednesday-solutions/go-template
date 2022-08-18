c52b651e09ebaae47c6e780d553e21104831db55
Mac <mohammed.ali.chherawalla@gmail.com>
2022-08-18 15:34:29 +0530

    feat: add more tests

--- pkg/utl/cnvrttogql/cnvrttogql_test.go
+++ pkg/utl/cnvrttogql/cnvrttogql_test.go
@@ -5,6 +5,9 @@ import (
 	"go-template/models"
 	"reflect"
 	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 func TestUsersToGraphQlUsers(t *testing.T) {
@@ -34,6 +37,45 @@ func TestUsersToGraphQlUsers(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := UsersToGraphQlUsers(tt.args.u, 1); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("UsersToGraphQlUsers() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRoleToGraphqlRole(t *testing.T) {
+	type args struct {
+		u *models.Role
+	}
+	tests := []struct {
+		name string
+		args args
+		want *graphql.Role
+	}{
+		{
+			name: "Success",
+			args: args{
+				u: &models.Role{
+					ID: 1,
+				},
+			},
+			want: &graphql.Role{
+				ID: "1",
+			},
+		},
+	}
+
+	db, _, err := sqlmock.New()
+	if err != nil {
+		if err != nil {
+			t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+		}
+	}
+	boil.SetDB(db)
+	defer db.Close()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := RoleToGraphqlRole(tt.args.u, 1); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("RoleToGraphqlRole() = %v, want %v", got, tt.want)
 			}
 		})
 	}
