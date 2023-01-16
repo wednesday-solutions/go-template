7c5bba533d5620e012eb5a9fe4ea7fa568b73325
Kshitij Gang <kshitij@wednesday.is>
2023-01-16 17:37:40 +0530

    fix(copilot-deployment) made changes to copilot deployment script and added precautions to readme

--- scripts/setup-ecs.sh
+++ scripts/setup-ecs.sh
@@ -4,6 +4,12 @@ copilot init -a "$1" -t "Load Balanced Web Service" -n "$1-$2-svc" -d ./Dockerfi
 
 copilot env init --name $2 --profile default --default-config
 
+copilot env deploy --name $2
+
 copilot storage init -n "$1-$2-cluster" -t Aurora -w "$1-$2-svc" --engine PostgreSQL --initial-db "$1_$2_db"
 
-copilot deploy --name "$1-$2-svc" -e "$2"
+copilot storage init -n "$1-$2-bucket" -t S3 
+
+aws s3 cp .env "s3://$1-$2-bucket/develop/"
+
+# copilot deploy --name "$1-$2-svc" -e "$2"
