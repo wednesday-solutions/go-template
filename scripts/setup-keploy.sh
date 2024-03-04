04c2c267e81cb236891eaea010a71c7a7491c38e
HIMANSHU <himanshu@RACITs-MacBook-Pro.local>
2024-03-04 22:59:12 +0530

    Feat: Update Setup-keploy.sh

--- scripts/setup-keploy.sh
+++ scripts/setup-keploy.sh
@@ -2,3 +2,5 @@
  curl -O https://raw.githubusercontent.com/keploy/keploy/main/keploy.sh && source keploy.sh
  keploy
  docker volume create --driver local --opt type=debugfs --opt device=debugfs debugfs
+ set -a && source .env.docker && set +a
+ docker build -t go-template-app:1.0 .
