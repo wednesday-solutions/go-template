925d53dba5db94bb16f96945201b0d86f5274e90
HIMANSHU <himanshu@wednesday.is>
2024-04-01 15:52:51 +0530

    role mutation resolver test comments resolved

--- resolver/role_mutations.resolvers_test.go
+++ resolver/role_mutations.resolvers_test.go
@@ -5,14 +5,12 @@ import (
 	"errors"
 	"go-template/daos"
 	"go-template/internal/constants"
-	"go-template/internal/middleware/auth"
 	"go-template/models"
 	"go-template/pkg/utl/rediscache"
 	"go-template/resolver"
 	"testing"
 
 	"github.com/agiledragon/gomonkey/v2"
-	null "github.com/volatiletech/null/v8"
 
 	fm "go-template/gqlmodels"
 
@@ -87,7 +85,15 @@ func successCase() createRoleType {
 			Name:        UserRoleName,
 		}},
 		wantErr: false,
-		init:    func() *gomonkey.Patches { return nil },
+		init: func() *gomonkey.Patches {
+			return gomonkey.ApplyFunc(daos.CreateRole,
+				func(role models.Role, ctx context.Context) (models.Role, error) {
+					return models.Role{
+						AccessLevel: int(constants.SuperAdminRole),
+						Name:        SuperAdminRoleName,
+					}, nil
+				})
+		},
 	}
 }
 
@@ -116,38 +122,6 @@ func loadTestCases() []createRoleType {
 		errorFromCreateRoleCase(),
 	}
 }
-func applyUserIdPatch() *gomonkey.Patches {
-	return gomonkey.ApplyFunc(auth.UserIDFromContext,
-		func(ctx context.Context) int {
-			return 1
-		})
-}
-func applyGetUserPatch() *gomonkey.Patches {
-	return gomonkey.ApplyFunc(rediscache.GetUser,
-		func(userID int, ctx context.Context) (*models.User, error) {
-			return &models.User{
-				RoleID: null.IntFrom(1),
-			}, nil
-		})
-}
-func applyGetRolePatch() *gomonkey.Patches {
-	return gomonkey.ApplyFunc(rediscache.GetRole,
-		func(roleID int, ctx context.Context) (*models.Role, error) {
-			return &models.Role{
-				AccessLevel: int(constants.SuperAdminRole),
-				Name:        SuperAdminRoleName,
-			}, nil
-		})
-}
-func applyCreateRolePatch() *gomonkey.Patches {
-	return gomonkey.ApplyFunc(daos.CreateRole,
-		func(role models.Role, ctx context.Context) (models.Role, error) {
-			return models.Role{
-				AccessLevel: int(constants.SuperAdminRole),
-				Name:        SuperAdminRoleName,
-			}, nil
-		})
-}
 
 // TestCreateRole tests the CreateRole mutation function.
 func TestCreateRole(
@@ -157,27 +131,16 @@ func TestCreateRole(
 	cases := loadTestCases()
 	// Create a new resolver instance.
 	resolver1 := resolver.Resolver{}
-
 	// Loop through each test case.
 	for _, tt := range cases {
 		// Mocking rediscache.GetUserID function
-		patchUserID := applyUserIdPatch()
-		// Mocking rediscache.GetUser function
-		patchGetUser := applyGetUserPatch()
-		// Mocking rediscache.GetRole function
-		patchGetRole := applyGetRolePatch()
-		// Mocking daos.CreateRole function
-		patchCreateRole := applyCreateRolePatch()
-		// Defer resetting of the monkey patches.
-		defer patchUserID.Reset()
-		defer patchGetUser.Reset()
-		defer patchGetRole.Reset()
-		defer patchCreateRole.Reset()
 		t.Run(tt.name,
 			func(t *testing.T) {
 				// Apply additional monkey patches based on test case name.
 				patch := tt.init()
-				defer patch.Reset()
+				if patch != nil {
+					defer patch.Reset()
+				}
 				// Create a new context
 				c := context.Background()
 				// Call the resolver function
@@ -185,9 +148,10 @@ func TestCreateRole(
 				// Check if the error matches the expected error
 				if tt.wantErr {
 					assert.NotNil(t, err)
+				} else {
+					assert.Equal(t, tt.wantResp, response)
 				}
 				// Check if the response matches the expected response
-				assert.Equal(t, tt.wantResp, response)
 			})
 	}
 }
