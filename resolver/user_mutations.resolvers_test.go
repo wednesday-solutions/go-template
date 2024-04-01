40302643e2c999113ccfbc88900bfd4835499e95
HIMANSHU <himanshu@wednesday.is>
2024-04-01 13:18:53 +0530

    user mutaion tests refactor

--- resolver/user_mutations.resolvers_test.go
+++ resolver/user_mutations.resolvers_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/agiledragon/gomonkey/v2"
 	"github.com/stretchr/testify/assert"
+	"github.com/volatiletech/sqlboiler/v4/boil"
 )
 
 type AnyTime struct{}
@@ -55,58 +56,92 @@ func expectInsertUser(mock sqlmock.Sqlmock, mockUser models.User) {
 		).
 		WillReturnRows(rows)
 }
-func getCreateUserTestCase() []struct {
-	name     string
-	req      fm.UserCreateInput
-	wantResp *fm.User
-	wantErr  bool
-} {
-	cases := []struct {
-		name     string
-		req      fm.UserCreateInput
-		wantResp *fm.User
-		wantErr  bool
-	}{
-		{
-			name:    ErrorFromCreateUser,
-			req:     fm.UserCreateInput{},
-			wantErr: true,
+
+type createUserType struct {
+	name      string
+	req       fm.UserCreateInput
+	wantResp  *fm.User
+	wantErr   bool
+	init      func() *gomonkey.Patches
+	initmocks func(mock sqlmock.Sqlmock, mockUser models.User)
+}
+
+func errorFromCreateUserCase() createUserType {
+	return createUserType{
+		name:    ErrorFromCreateUser,
+		req:     fm.UserCreateInput{},
+		wantErr: true,
+		init:    func() *gomonkey.Patches { return nil },
+		initmocks: func(mock sqlmock.Sqlmock, mockUser models.User) {
+			mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO "users"`)).
+				WithArgs().
+				WillReturnError(fmt.Errorf(""))
 		},
-		{
-			name:    ErrorFromThrottleCheck,
-			req:     fm.UserCreateInput{},
-			wantErr: true,
+	}
+}
+
+func errorFromThrottleCheck() createUserType {
+	return createUserType{
+		name:    ErrorFromThrottleCheck,
+		req:     fm.UserCreateInput{},
+		wantErr: true,
+		init: func() *gomonkey.Patches {
+			return gomonkey.ApplyFunc(throttle.Check, func(ctx context.Context, limit int, dur time.Duration) error {
+				return fmt.Errorf("Internal error")
+			})
 		},
-		{
-			name:    ErrorFromConfig,
-			req:     fm.UserCreateInput{},
-			wantErr: true,
+		initmocks: expectInsertUser,
+	}
+}
+func errorFromCreateUserConfigCase() createUserType {
+	return createUserType{
+		name:    ErrorFromConfig,
+		req:     fm.UserCreateInput{},
+		wantErr: true,
+		init: func() *gomonkey.Patches {
+			return gomonkey.ApplyFunc(config.Load, func() (*config.Configuration, error) {
+				return nil, fmt.Errorf("error in loading config")
+			})
 		},
-		{
-			name: SuccessCase,
-			req: fm.UserCreateInput{
-				FirstName: testutls.MockUser().FirstName.String,
-				LastName:  testutls.MockUser().LastName.String,
-				Username:  testutls.MockUser().Username.String,
-				Email:     testutls.MockUser().Email.String,
-				RoleID:    fmt.Sprint(testutls.MockUser().RoleID.Int),
-			},
-			wantResp: &fm.User{
-				ID:                 fmt.Sprint(testutls.MockUser().ID),
-				Email:              convert.NullDotStringToPointerString(testutls.MockUser().Email),
-				FirstName:          convert.NullDotStringToPointerString(testutls.MockUser().FirstName),
-				LastName:           convert.NullDotStringToPointerString(testutls.MockUser().LastName),
-				Username:           convert.NullDotStringToPointerString(testutls.MockUser().Username),
-				Mobile:             convert.NullDotStringToPointerString(testutls.MockUser().Mobile),
-				Address:            convert.NullDotStringToPointerString(testutls.MockUser().Address),
-				Active:             convert.NullDotBoolToPointerBool(testutls.MockUser().Active),
-				LastLogin:          convert.NullDotTimeToPointerInt(testutls.MockUser().LastLogin),
-				LastPasswordChange: convert.NullDotTimeToPointerInt(testutls.MockUser().LastPasswordChange),
-				DeletedAt:          convert.NullDotTimeToPointerInt(testutls.MockUser().DeletedAt),
-				UpdatedAt:          convert.NullDotTimeToPointerInt(testutls.MockUser().UpdatedAt),
-			},
-			wantErr: false,
+		initmocks: expectInsertUser,
+	}
+}
+
+func createUserSuccessCase() createUserType {
+	return createUserType{
+		name: SuccessCase,
+		req: fm.UserCreateInput{
+			FirstName: testutls.MockUser().FirstName.String,
+			LastName:  testutls.MockUser().LastName.String,
+			Username:  testutls.MockUser().Username.String,
+			Email:     testutls.MockUser().Email.String,
+			RoleID:    fmt.Sprint(testutls.MockUser().RoleID.Int),
 		},
+		wantResp: &fm.User{
+			ID:                 fmt.Sprint(testutls.MockUser().ID),
+			Email:              convert.NullDotStringToPointerString(testutls.MockUser().Email),
+			FirstName:          convert.NullDotStringToPointerString(testutls.MockUser().FirstName),
+			LastName:           convert.NullDotStringToPointerString(testutls.MockUser().LastName),
+			Username:           convert.NullDotStringToPointerString(testutls.MockUser().Username),
+			Mobile:             convert.NullDotStringToPointerString(testutls.MockUser().Mobile),
+			Address:            convert.NullDotStringToPointerString(testutls.MockUser().Address),
+			Active:             convert.NullDotBoolToPointerBool(testutls.MockUser().Active),
+			LastLogin:          convert.NullDotTimeToPointerInt(testutls.MockUser().LastLogin),
+			LastPasswordChange: convert.NullDotTimeToPointerInt(testutls.MockUser().LastPasswordChange),
+			DeletedAt:          convert.NullDotTimeToPointerInt(testutls.MockUser().DeletedAt),
+			UpdatedAt:          convert.NullDotTimeToPointerInt(testutls.MockUser().UpdatedAt),
+		},
+		wantErr:   false,
+		init:      func() *gomonkey.Patches { return nil },
+		initmocks: expectInsertUser,
+	}
+}
+func getCreateUserTestCase() []createUserType {
+	cases := []createUserType{
+		errorFromCreateUserCase(),
+		errorFromThrottleCheck(),
+		errorFromCreateUserConfigCase(),
+		createUserSuccessCase(),
 	}
 	return cases
 }
@@ -116,26 +151,9 @@ func TestCreateUser(t *testing.T) {
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
 			mock, cleanup, _ := testutls.SetupMockDB(t)
-
-			if tt.name == ErrorFromCreateUser {
-				mock.ExpectQuery(regexp.QuoteMeta(`INSERT INTO "users"`)).
-					WithArgs().
-					WillReturnError(fmt.Errorf(""))
-			} else {
-				expectInsertUser(mock, *testutls.MockUser())
-			}
-
-			if tt.name == ErrorFromThrottleCheck {
-				patch := gomonkey.ApplyFunc(throttle.Check, func(ctx context.Context, limit int, dur time.Duration) error {
-					return fmt.Errorf("Internal error")
-				})
-				defer patch.Reset()
-			}
-
-			if tt.name == ErrorFromConfig {
-				patch := gomonkey.ApplyFunc(config.Load, func() (*config.Configuration, error) {
-					return nil, fmt.Errorf("error in loading config")
-				})
+			tt.initmocks(mock, *testutls.MockUser())
+			patch := tt.init()
+			if patch != nil {
 				defer patch.Reset()
 			}
 			response, err := resolver.Mutation().CreateUser(context.Background(), tt.req)
@@ -148,22 +166,25 @@ func TestCreateUser(t *testing.T) {
 	}
 }
 
-func GetUpdateUserTestCase() []struct {
-	name     string
-	req      *fm.UserUpdateInput
-	wantResp *fm.User
-	wantErr  bool
-} {
-	cases := []struct {
-		name     string
-		req      *fm.UserUpdateInput
-		wantResp *fm.User
-		wantErr  bool
-	}{
+type updateUserType struct {
+	name      string
+	req       *fm.UserUpdateInput
+	wantResp  *fm.User
+	wantErr   bool
+	init      func()
+	initMocks func(mock sqlmock.Sqlmock)
+}
+
+func loadUpdateUserTestCases() []updateUserType {
+	return []updateUserType{
 		{
 			name:    ErrorFindingUser,
 			req:     &fm.UserUpdateInput{},
 			wantErr: true,
+			init:    func() {},
+			initMocks: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery(regexp.QuoteMeta(`UPDATE "users"`)).WithArgs().WillReturnError(fmt.Errorf(""))
+			},
 		},
 		{
 			name: ErrorUpdateUser,
@@ -174,6 +195,19 @@ func GetUpdateUserTestCase() []struct {
 				Address:   &testutls.MockUser().Address.String,
 			},
 			wantErr: true,
+			init: func() {
+				gomonkey.ApplyFunc(daos.UpdateUser,
+					func(user models.User, ctx context.Context) (models.User, error) {
+						return user, fmt.Errorf("error for update user")
+					})
+			},
+			initMocks: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"first_name"}).AddRow(testutls.MockUser().FirstName)
+				mock.ExpectQuery(regexp.QuoteMeta(`select * from "users"`)).WithArgs(0).WillReturnRows(rows)
+				// update users with new information
+				result := driver.Result(driver.RowsAffected(1))
+				mock.ExpectExec(regexp.QuoteMeta(`UPDATE "users"`)).WillReturnResult(result)
+			},
 		},
 		{
 			name: SuccessCase,
@@ -191,40 +225,37 @@ func GetUpdateUserTestCase() []struct {
 				Address:   &testutls.MockUser().Address.String,
 			},
 			wantErr: false,
-		},
-	}
-	return cases
-}
-func TestUpdateUser(
-	t *testing.T,
-) {
-	cases := GetUpdateUserTestCase()
-	resolver1 := resolver.Resolver{}
-	for _, tt := range cases {
-		t.Run(
-			tt.name,
-			func(t *testing.T) {
-				if tt.name == ErrorUpdateUser {
-					patch := gomonkey.ApplyFunc(daos.UpdateUser,
-						func(user models.User, ctx context.Context) (models.User, error) {
-							return user, fmt.Errorf("error for update user")
-						})
-					defer patch.Reset()
-				}
-				err := config.LoadEnv()
-				if err != nil {
-					log.Fatal(err)
-				}
-				mock, cleanup, _ := testutls.SetupMockDB(t)
-
-				if tt.name == ErrorFindingUser {
-					mock.ExpectQuery(regexp.QuoteMeta(`UPDATE "users"`)).WithArgs().WillReturnError(fmt.Errorf(""))
-				}
+			init:    func() {},
+			initMocks: func(mock sqlmock.Sqlmock) {
 				rows := sqlmock.NewRows([]string{"first_name"}).AddRow(testutls.MockUser().FirstName)
 				mock.ExpectQuery(regexp.QuoteMeta(`select * from "users"`)).WithArgs(0).WillReturnRows(rows)
 				// update users with new information
 				result := driver.Result(driver.RowsAffected(1))
 				mock.ExpectExec(regexp.QuoteMeta(`UPDATE "users"`)).WillReturnResult(result)
+			},
+		},
+	}
+}
+
+func TestUpdateUser(
+	t *testing.T,
+) {
+	cases := loadUpdateUserTestCases()
+	resolver1 := resolver.Resolver{}
+	for _, tt := range cases {
+		t.Run(
+			tt.name,
+			func(t *testing.T) {
+				tt.init()
+				err := config.LoadEnv()
+				if err != nil {
+					log.Fatal(err)
+				}
+				mock, cleanup, _ := testutls.SetupMockDB(t)
+				oldDB := boil.GetDB()
+				defer cleanup()
+				boil.SetDB(oldDB)
+				tt.initMocks(mock)
 				c := context.Background()
 				ctx := context.WithValue(c, testutls.UserKey, testutls.MockUser())
 				response, err := resolver1.Mutation().UpdateUser(ctx, tt.req)
@@ -233,29 +264,64 @@ func TestUpdateUser(
 					assert.Equal(t, tt.wantResp, response)
 				}
 				assert.Equal(t, tt.wantErr, err != nil)
-				cleanup()
 			},
 		)
 	}
 }
 
 func GetDeleteTestCases() []struct {
-	name     string
-	wantResp *fm.UserDeletePayload
-	wantErr  bool
+	name      string
+	wantResp  *fm.UserDeletePayload
+	wantErr   bool
+	init      func() *gomonkey.Patches
+	initMocks func(mock sqlmock.Sqlmock)
 } {
 	cases := []struct {
-		name     string
-		wantResp *fm.UserDeletePayload
-		wantErr  bool
+		name      string
+		wantResp  *fm.UserDeletePayload
+		wantErr   bool
+		init      func() *gomonkey.Patches
+		initMocks func(mock sqlmock.Sqlmock)
 	}{
 		{
 			name:    ErrorFindingUser,
 			wantErr: true,
+			init:    func() *gomonkey.Patches { return nil },
+			initMocks: func(mock sqlmock.Sqlmock) {
+				mock.ExpectQuery(regexp.QuoteMeta(`select * from "users" where "id"=$1`)).
+					WithArgs().
+					WillReturnError(fmt.Errorf(""))
+				rows := sqlmock.NewRows([]string{"id"}).
+					AddRow(1)
+				mock.ExpectQuery(regexp.QuoteMeta(`select * from "users" where "id"=$1`)).
+					WithArgs().
+					WillReturnRows(rows)
+				// delete user
+				result := driver.Result(driver.RowsAffected(1))
+				mock.ExpectExec(regexp.QuoteMeta(`DELETE FROM "users" WHERE "id"=$1`)).
+					WillReturnResult(result)
+			},
 		},
 		{
 			name:    ErrorDeleteUser,
 			wantErr: true,
+			init: func() *gomonkey.Patches {
+				return gomonkey.ApplyFunc(daos.DeleteUser,
+					func(user models.User, ctx context.Context) (int64, error) {
+						return 0, fmt.Errorf("error for delete user")
+					})
+			},
+			initMocks: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"id"}).
+					AddRow(1)
+				mock.ExpectQuery(regexp.QuoteMeta(`select * from "users" where "id"=$1`)).
+					WithArgs().
+					WillReturnRows(rows)
+				// delete user
+				result := driver.Result(driver.RowsAffected(1))
+				mock.ExpectExec(regexp.QuoteMeta(`DELETE FROM "users" WHERE "id"=$1`)).
+					WillReturnResult(result)
+			},
 		},
 		{
 			name: SuccessCase,
@@ -263,6 +329,18 @@ func GetDeleteTestCases() []struct {
 				ID: "0",
 			},
 			wantErr: false,
+			init:    func() *gomonkey.Patches { return nil },
+			initMocks: func(mock sqlmock.Sqlmock) {
+				rows := sqlmock.NewRows([]string{"id"}).
+					AddRow(1)
+				mock.ExpectQuery(regexp.QuoteMeta(`select * from "users" where "id"=$1`)).
+					WithArgs().
+					WillReturnRows(rows)
+				// delete user
+				result := driver.Result(driver.RowsAffected(1))
+				mock.ExpectExec(regexp.QuoteMeta(`DELETE FROM "users" WHERE "id"=$1`)).
+					WillReturnResult(result)
+			},
 		},
 	}
 	return cases
@@ -277,33 +355,16 @@ func TestDeleteUser(
 		t.Run(
 			tt.name,
 			func(t *testing.T) {
-				if tt.name == ErrorDeleteUser {
-					patch := gomonkey.ApplyFunc(daos.DeleteUser,
-						func(user models.User, ctx context.Context) (int64, error) {
-							return 0, fmt.Errorf("error for delete user")
-						})
+				patch := tt.init()
+				if patch != nil {
 					defer patch.Reset()
 				}
 				mock, cleanup, _ := testutls.SetupMockDB(t)
-				if tt.name == ErrorFindingUser {
-					mock.ExpectQuery(regexp.QuoteMeta(`select * from "users" where "id"=$1`)).
-						WithArgs().
-						WillReturnError(fmt.Errorf(""))
-				}
 				// get user by id
-				rows := sqlmock.NewRows([]string{"id"}).
-					AddRow(1)
-				mock.ExpectQuery(regexp.QuoteMeta(`select * from "users" where "id"=$1`)).
-					WithArgs().
-					WillReturnRows(rows)
-				// delete user
-				result := driver.Result(driver.RowsAffected(1))
-				mock.ExpectExec(regexp.QuoteMeta(`DELETE FROM "users" WHERE "id"=$1`)).
-					WillReturnResult(result)
+				tt.initMocks(mock)
 				c := context.Background()
 				ctx := context.WithValue(c, testutls.UserKey, testutls.MockUser())
-				response, err := resolver1.Mutation().
-					DeleteUser(ctx)
+				response, err := resolver1.Mutation().DeleteUser(ctx)
 				if tt.wantResp != nil {
 					assert.Equal(t, tt.wantResp, response)
 				}
