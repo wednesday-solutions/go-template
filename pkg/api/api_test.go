9b1ea9eda18ba983168bbd780867109ecaedc23a
HIMANSHU <himanshu@wednesday.is>
2024-04-03 21:47:09 +0530

    nater api test

--- pkg/api/api_test.go
+++ pkg/api/api_test.go
@@ -6,6 +6,7 @@ import (
 	"database/sql"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
@@ -24,7 +25,6 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/agiledragon/gomonkey/v2"
-	. "github.com/agiledragon/gomonkey/v2"
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq"
@@ -37,7 +37,7 @@ var graphQLPathname = "/graphql"
 type args struct {
 	cfg *config.Configuration
 }
-type testCases struct {
+type testStartServerType struct {
 	name                         string
 	args                         args
 	wantErr                      bool
@@ -47,201 +47,197 @@ type testCases struct {
 	optionsTransportCalled       bool
 	multipartFormTransportCalled bool
 	websocketTransportCalled     bool
+	init                         func(e *echo.Echo, tt testStartServerType) *gomonkey.Patches
 }
 
-func TestStart(t *testing.T) {
-	tests := initializeTestCases()
-
-	patches := applyPatches()
-	defer patches.Reset()
-
-	mockEchoServer, _ := prepareMocks()
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			setupMockFunctions()
-
-			if hasTransportCalls(tt) {
-				checkTransportCalls(tt)
-			} else {
-				checkGraphQLServer(t, tt, mockEchoServer)
-			}
-		})
-	}
-}
-
-func initializeTestCases() []testCases {
-	return []testCases{
-		{
-			name: "Success",
-			args: args{
-				cfg: testutls.MockConfig(),
-			},
-			wantErr: false,
+func testStartServerSuccessCase() testStartServerType {
+	return testStartServerType{
+		name: "Success",
+		args: args{
+			cfg: testutls.MockConfig(),
 		},
-		{
-			name: "Test_AddTransport",
-			args: args{
-				cfg: testutls.MockConfig(),
-			},
-			wantErr:                      false,
-			getTransportCalled:           false,
-			postTransportCalled:          false,
-			optionsTransportCalled:       false,
-			multipartFormTransportCalled: false,
-			websocketTransportCalled:     false,
+		setDbCalled: true,
+		wantErr:     false,
+		init: func(e *echo.Echo, tt testStartServerType) *gomonkey.Patches {
+			return gomonkey.ApplyFunc(os.Getenv, func(key string) (value string) {
+				if key == "JWT_SECRET" {
+					return testutls.MockJWTSecret
+				}
+				return ""
+			}).ApplyFunc(sql.Open, func(driverName string, dataSourceName string) (*sql.DB, error) {
+				fmt.Print("sql.Open called\n")
+				return nil, nil
+			}).ApplyFunc(server.Start, func(e *echo.Echo, cfg *server.Config) {
+				fmt.Print("Fake server started\n")
+			}).ApplyFunc(server.New, func() *echo.Echo {
+				return e
+			}).ApplyFunc(boil.SetDB, func(db boil.Executor) {
+				fmt.Print("boil.SetDB called\n", tt.setDbCalled)
+			})
 		},
 	}
 }
-
-func applyPatches() *gomonkey.Patches {
-	return ApplyFunc(os.Getenv, mockGetenv).
-		ApplyFunc(sql.Open, mockSqlOpen).
-		ApplyFunc(server.Start, mockServerStart).
-		ApplyFunc(server.New, mockServerNew)
-}
-
-func mockGetenv(key string) string {
-	if key == "JWT_SECRET" {
-		return testutls.MockJWTSecret
-	}
-	return ""
-}
-
-func mockSqlOpen(driverName string, dataSourceName string) (*sql.DB, error) {
-	fmt.Print("sql.Open called\n")
-	return nil, nil
-}
-
-func mockServerStart(e *echo.Echo, cfg *server.Config) {
-	fmt.Print("Fake server started\n")
-}
-
-func mockServerNew() *echo.Echo {
-	return echo.New()
-}
-
-func prepareMocks() (*echo.Echo, *handler.Server) {
-	e := echo.New()
-	graphqlHandler := handler.New(graphql.NewExecutableSchema(graphql.Config{
-		Resolvers: &resolver.Resolver{Observers: make(map[string]chan *graphql.User)},
-	}))
-	return e, graphqlHandler
-}
-
-func setupMockFunctions() {
-	ApplyFunc(boil.SetDB, mockSetDB)
-}
-
-func mockSetDB(db boil.Executor, tt testCases) {
-	fmt.Print("boil.SetDB called\n")
-	tt.setDbCalled = true
-}
-
-func hasTransportCalls(tt testCases) bool {
-	return tt.getTransportCalled || tt.postTransportCalled ||
-		tt.optionsTransportCalled || tt.multipartFormTransportCalled
-}
-
-func checkTransportCalls(tt testCases) {
-	observers := map[string]chan *graphql.User{}
-	graphqlHandler := handler.New(graphql.NewExecutableSchema(graphql.Config{
-		Resolvers: &resolver.Resolver{Observers: observers},
-	}))
-	if tt.getTransportCalled || tt.postTransportCalled ||
-		tt.optionsTransportCalled || tt.multipartFormTransportCalled {
-		ApplyMethod(reflect.TypeOf(graphqlHandler), "AddTransport", func(s *handler.Server, t graphql2.Transport) {
-			transportGET := transport.GET{}
-			transportMultipartForm := transport.MultipartForm{}
-			transportPOST := transport.POST{}
-			transportWebsocket := transport.Websocket{
-				KeepAlivePingInterval: 10 * time.Second,
-				InitFunc: func(ctx context.Context, initPayload transport.InitPayload) (context.Context, error) {
-					return ctx, nil
-				},
-				Upgrader: websocket.Upgrader{
-					CheckOrigin: func(r *http.Request) bool {
-						return true
+func testWithTransportCase() testStartServerType {
+	return testStartServerType{
+		name: "Test_AddTransport",
+		args: args{
+			cfg: testutls.MockConfig(),
+		},
+		wantErr:                      false,
+		getTransportCalled:           true,
+		postTransportCalled:          true,
+		optionsTransportCalled:       true,
+		multipartFormTransportCalled: true,
+		websocketTransportCalled:     true,
+		init: func(e *echo.Echo, tt testStartServerType) *gomonkey.Patches {
+			observers := map[string]chan *graphql.User{}
+			graphqlHandler := handler.New(graphql.NewExecutableSchema(graphql.Config{
+				Resolvers: &resolver.Resolver{Observers: observers},
+			}))
+			return gomonkey.ApplyFunc(os.Getenv, func(key string) (value string) {
+				if key == "JWT_SECRET" {
+					return testutls.MockJWTSecret
+				}
+				return ""
+			}).ApplyFunc(sql.Open, func(driverName string, dataSourceName string) (*sql.DB, error) {
+				fmt.Print("sql.Open called\n")
+				return nil, nil
+			}).ApplyFunc(server.Start, func(e *echo.Echo, cfg *server.Config) {
+				fmt.Print("Fake server started\n")
+			}).ApplyFunc(server.New, func() *echo.Echo {
+				return e
+			}).ApplyFunc(boil.SetDB, func(db boil.Executor) {
+				//mocking  boil.setdb
+			}).ApplyMethod(reflect.TypeOf(graphqlHandler), "AddTransport", func(s *handler.Server, t graphql2.Transport) {
+				transportGET := transport.GET{}
+				transportMultipartForm := transport.MultipartForm{}
+				transportPOST := transport.POST{}
+				transportWebsocket := transport.Websocket{
+					KeepAlivePingInterval: 10 * time.Second,
+					InitFunc: func(ctx context.Context, initPayload transport.InitPayload) (context.Context, error) {
+						return ctx, nil
 					},
-				},
-			}
-			if t == transportGET {
-				tt.getTransportCalled = true
-			}
-			if t == transportMultipartForm {
-				tt.multipartFormTransportCalled = true
-			}
-			if t == transportPOST {
-				tt.postTransportCalled = true
-			}
-			if reflect.TypeOf(t) == reflect.TypeOf(transportWebsocket) {
-				tt.websocketTransportCalled = true
-			}
-		})
+					Upgrader: websocket.Upgrader{
+						CheckOrigin: func(r *http.Request) bool {
+							return true
+						},
+					},
+				}
+				if t == transportGET {
+					tt.getTransportCalled = true
+				}
+				if t == transportMultipartForm {
+					tt.multipartFormTransportCalled = true
+				}
+				if t == transportPOST {
+					tt.postTransportCalled = true
+				}
+				if reflect.TypeOf(t) == reflect.TypeOf(transportWebsocket) {
+					tt.websocketTransportCalled = true
+				}
+			})
+		},
 	}
 }
-func checkGraphQLServer(t *testing.T, tt testCases, e *echo.Echo) {
-	jsonRes, err := testutls.MakeRequest(testutls.RequestParameters{
-		E:           e,
-		Pathname:    graphQLPathname,
-		HttpMethod:  "POST",
-		RequestBody: testutls.MockWhitelistedQuery,
-		IsGraphQL:   false,
-	})
-	if err != nil {
-		t.Fatalf("Failed to make request to GraphQL server: %v", err)
+func loadTestStartCases() []testStartServerType {
+	return []testStartServerType{
+		testStartServerSuccessCase(),
+		testWithTransportCase(),
 	}
+}
 
-	// Assert that database was set
-	assert.True(t, tt.setDbCalled, "Expected database to be set")
-
-	// Assert GraphQL schema is returned
-	assert.NotNil(t, jsonRes["data"].(map[string]interface{})["__schema"],
-		"Expected GraphQL schema to be returned")
-
-	// Simulate request to the GraphQL playground
+func checkGraphQLGetResponse(e *echo.Echo) (*http.Response, error) {
 	_, res, err := testutls.SimpleMakeRequest(testutls.RequestParameters{
 		E:          e,
 		Pathname:   "/playground",
 		HttpMethod: "GET",
 		IsGraphQL:  false,
 	})
-	if err != nil {
-		t.Fatalf("Failed to make request to GraphQL playground: %v", err)
+	return res, err
+}
+func checkGraphQLPostResponse(e *echo.Echo) (map[string]interface{}, error) {
+	res, err := testutls.MakeRequest(testutls.RequestParameters{
+		E:           e,
+		Pathname:    graphQLPathname,
+		HttpMethod:  "POST",
+		RequestBody: testutls.MockWhitelistedQuery,
+		IsGraphQL:   false,
+	})
+	return res, err
+}
+func TestStart(t *testing.T) {
+	tests := loadTestStartCases()
+	e := echo.New()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			runTest(t, tt, e)
+		})
 	}
+}
 
-	bodyBytes, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Fatalf("Failed to read response body: %v", err)
+func runTest(t *testing.T, tt testStartServerType, e *echo.Echo) {
+	patches := tt.init(e, tt)
+	if tt.getTransportCalled || tt.postTransportCalled ||
+		tt.optionsTransportCalled || tt.multipartFormTransportCalled {
+		testWithTransportCalls(t, tt, e)
+	} else {
+		testWithoutTransportCalls(t, tt, e)
 	}
+	patches.Reset()
+}
 
-	// Assert that GraphQL playground is returned
-	assert.Contains(t, string(bodyBytes), "GraphiQL.createFetcher",
-		"Expected GraphQL playground to be returned")
+func testWithTransportCalls(t *testing.T, tt testStartServerType, e *echo.Echo) {
+	_, err := Start(tt.args.cfg)
+	assertError(t, err, tt.wantErr)
+	assert.Equal(t, tt.getTransportCalled, true)
+	assert.Equal(t, tt.multipartFormTransportCalled, true)
+	assert.Equal(t, tt.postTransportCalled, true)
+	assert.Equal(t, tt.websocketTransportCalled, true)
+}
 
-	// Connect to the WebSocket endpoint
+func testWithoutTransportCalls(t *testing.T, tt testStartServerType, e *echo.Echo) {
+	_, err := Start(tt.args.cfg)
+	assertError(t, err, tt.wantErr)
+	checkGraphQLResponse(t, e)
+	checkWebsocketConnection(t, e)
+}
+
+func assertError(t *testing.T, err error, wantErr bool) {
+	if err != nil != wantErr {
+		t.Errorf("Error occurred = %v, wantErr %v", err, wantErr)
+	}
+}
+
+func checkGraphQLResponse(t *testing.T, e *echo.Echo) {
+	jsonRes, err := checkGraphQLPostResponse(e)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.Equal(t, true, jsonRes["data"].(map[string]interface{})["__schema"] != nil, "Schema is nil")
+	res, err := checkGraphQLGetResponse(e)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bodyBytes, _ := io.ReadAll(res.Body)
+	assert.Contains(t, string(bodyBytes), "GraphiQL.createFetcher", "Playground not found")
+}
+
+func checkWebsocketConnection(t *testing.T, e *echo.Echo) {
 	ts := httptest.NewServer(e)
-	defer ts.Close()
 	u := "ws" + strings.TrimPrefix(ts.URL+graphQLPathname, "http")
 	ws, _, err := websocket.DefaultDialer.Dial(u, nil)
 	if err != nil {
-		t.Fatalf("Failed to connect to WebSocket: %v", err)
+		t.Fatalf("%v", err)
 	}
-	defer ws.Close()
-
-	// Send connection initiation message
 	if err := ws.WriteMessage(websocket.TextMessage, []byte(`{"type":"connection_init","payload":`+
 		`{"authorization":"bearer ABC"}}`)); err != nil {
-		t.Fatalf("Failed to send connection initiation message: %v", err)
+		t.Fatalf("%v", err)
 	}
-
-	// Read response from WebSocket
 	_, p, err := ws.ReadMessage()
 	if err != nil {
-		t.Fatalf("Failed to read WebSocket message: %v", err)
+		t.Fatalf("%v", err)
 	}
-
-	// Assert that connection acknowledgement is received
-	assert.Contains(t, string(p), "{\"type\":\"connection_ack\"}\n",
-		"Expected connection acknowledgement from WebSocket")
+	assert.Contains(t, string(p), "{\"type\":\"connection_ack\"}\n", "Connection ack not found")
+	ts.Close()
+	ws.Close()
 }
